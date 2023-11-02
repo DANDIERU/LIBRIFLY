a16d7a6421c06841b54151f38d6800ba3c84df27
Daniel Caicedo <97268461+DANDIERU@users.noreply.github.com>
2023-11-01 23:33:46 -0500

    [ADD]My reading Lists screen pre structure

--- src/screens/My_Reading_lists.ts
+++ src/screens/My_Reading_lists.ts
@@ -1,3 +1,7 @@
+import { AttributeReading } from "../components/solidReading/solidReading";
+import "../components/export"
+
+
 class MyReadingList extends HTMLElement {
     constructor() {
       super();
@@ -11,6 +15,19 @@ class MyReadingList extends HTMLElement {
   
     render() {
       if (this.shadowRoot) {
+
+        const link = this.ownerDocument.createElement("link")
+        link.setAttribute("rel", "stylesheet")
+        link.setAttribute("href", "src/screens/My_Reading_lists.css")
+        this.shadowRoot?.appendChild(link);
+
+        const readingDiv = this.ownerDocument.createElement("solid-reading")
+        readingDiv.classList.add("reading-div")
+        readingDiv.setAttribute(AttributeReading.main_reading, "My reading List")
+        readingDiv.setAttribute(AttributeReading.empty_desc, "Oh, you don't have lists yet.Create your first list")
+        readingDiv.setAttribute(AttributeReading.btn_create, "Create list")
+        readingDiv.setAttribute(AttributeReading.img_main, "src/img/girlreadinglist.png")
+        this.shadowRoot.appendChild(readingDiv)
         
       }
     }
