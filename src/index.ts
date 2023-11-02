13e5c37e831b1cd3d1c26d03ab91a295a48e9637
THEEVEEND <128189477+THEEVEEND@users.noreply.github.com>
2023-11-01 22:21:49 -0500

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-//import * as components from "./components/export";
+import * as components from "./components/export";
+import { navigate } from "./store/actions";
 import "./components/export"
 import "./screens/Book_Details"
 import "./screens/Dashboard"
@@ -28,58 +29,59 @@ class AppContainer extends  HTMLElement {
 
     render() {
         if(this.shadowRoot){
+            this.shadowRoot.innerHTML = `<h1>hola</h1>`;
             switch (appState.screen) {
-                case screens.SIGN_IN : 
+                case screens.SIGN_IN :
                 const signIn = this.ownerDocument.createElement("sign-in")
                 this.shadowRoot?.appendChild(signIn)
                 break;
 
-                case screens.REGISTER : 
-                const register = this.ownerDocument.createElement("register")
+                case screens.REGISTER :
+                const register = this.ownerDocument.createElement("app-register")
                 this.shadowRoot?.appendChild(register)
                 break;
 
-                case screens.DASHBOARD : 
+                case screens.DASHBOARD :
                 const dashboard = this.ownerDocument.createElement("app-dashboard")
                 this.shadowRoot?.appendChild(dashboard)
                 break;
 
-                case screens.MY_READING_LISTS : 
+                case screens.MY_READING_LISTS :
                 const myReadingList = this.ownerDocument.createElement("my-readinglist")
                 this.shadowRoot?.appendChild(myReadingList)
                 break;
 
-                case screens.BOOK_DETAILS : 
+                case screens.BOOK_DETAILS :
                 const bookDetails = this.ownerDocument.createElement("book-details")
                 this.shadowRoot?.appendChild(bookDetails)
                 break;
 
-                case screens.LIST_DETAILS : 
+                case screens.LIST_DETAILS :
                 const listDetails = this.ownerDocument.createElement("list-details")
                 this.shadowRoot?.appendChild(listDetails)
                 break;
 
-                case screens.GENRE_DETAILS : 
+                case screens.GENRE_DETAILS :
                 const denreDetails = this.ownerDocument.createElement("genre-details")
                 this.shadowRoot?.appendChild(denreDetails)
                 break;
 
-                case screens.SEARCH_DETAILS : 
+                case screens.SEARCH_DETAILS :
                 const searchDetails = this.ownerDocument.createElement("search-details")
                 this.shadowRoot?.appendChild(searchDetails)
                 break;
 
-                case screens.USER_INFO : 
+                case screens.USER_INFO :
                 const userInfo = this.ownerDocument.createElement("user-info")
                 this.shadowRoot?.appendChild(userInfo)
                 break;
 
-                case screens.PROFILE : 
+                case screens.PROFILE :
                 const profile = this.ownerDocument.createElement("my-profile")
                 this.shadowRoot?.appendChild(profile)
                 break;
 
-                case screens.EDIT_PROFILE : 
+                case screens.EDIT_PROFILE :
                 const editProfile = this.ownerDocument.createElement("edit-profile")
                 this.shadowRoot?.appendChild(editProfile)
                 break;
