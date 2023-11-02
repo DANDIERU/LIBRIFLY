3af3762bab900614cdc4817afbaa2fa178c4a2b6
Daniel Caicedo <97268461+DANDIERU@users.noreply.github.com>
2023-11-02 04:25:30 -0500

    [ADD]Navbar push on Dashboard

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,7 @@ import { screens } from "../types/navigation";
 
 const emptyState = {
     //pantalla inicial
-  screen: screens.MY_READING_LISTS,
+  screen: screens.DASHBOARD,
 };
 
 export let appState = Storage.get<AppState>({
