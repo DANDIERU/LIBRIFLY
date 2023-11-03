5e2972405e7bc8d1e9a772be05489ae4572385bc
THEEVEEND <theeveend@gmail.com>
2023-11-02 23:04:56 -0500

    [ADD]Icon

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,7 @@ import { screens } from "../types/navigation";
 
 const emptyState = {
     //pantalla inicial lo que se muestra 
-  screen: screens.SIGN_IN,
+  screen: screens.PROFILE,
 };
 
 export let appState = Storage.get<AppState>({
